1e417f32b8989f50d6fc3015dbb52d54d549d699
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-10 12:04:28 -0600

    Test 3 ageOnMars good fail

--- __test__/EarthAge.test.js
+++ __test__/EarthAge.test.js
@@ -4,11 +4,15 @@ describe('EarthAge', () => {
     
    
     test('should create a variable for earthYears', () =>{
-    let newAge = new EarthAge(37);
-    expect(newAge.earthYears).toBe(37);
+        let newAge = new EarthAge(37);
+        expect(newAge.earthYears).toBe(37);
 });
     test(' method should return newAge in Venus years', () =>{
-    let newAge = new EarthAge(37);
-    expect(newAge.ageOnVenus()).toBe(23);
+        let newAge = new EarthAge(37);
+        expect(newAge.ageOnVenus()).toBe(23);
 });
+    test('method should return newAge in Mars years', () =>{
+        let newAge = new EarthAge(37);
+        expect(newAge.ageOnMars()).toBe(19);
+    })
 });
--- src/js/EarthAge.js
+++ src/js/EarthAge.js
@@ -5,4 +5,7 @@ export class EarthAge {
     ageOnVenus() {
         return Math.round(this.earthYears * 0.62);
     }
+    ageOnMars() {
+        
+    }
 }
