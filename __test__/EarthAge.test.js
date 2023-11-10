6a85f019bddff5622f562719d471dfe8e8156746
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-10 11:37:08 -0600

    Test 2 good fail

--- __test__/EarthAge.test.js
+++ __test__/EarthAge.test.js
@@ -1,11 +1,14 @@
 import { EarthAge } from '../src/js/EarthAge.js'
 
 describe('EarthAge', () => {
-    let newAge = new EarthAge(37);
+    
    
     test('should create a variable for earthYears', () =>{
+    let newAge = new EarthAge(37);
     expect(newAge.earthYears).toBe(37);
 });
-    test(' method should return newAge in Venus years ')
+    test(' method should return newAge in Venus years', () =>{
+    let newAge = new EarthAge(37);
     expect(newAge.ageOnVenus()).toEqual(60);
+});
 });
