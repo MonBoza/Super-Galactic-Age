910c34c76cf88f4aec858ab76e114d6b2e4c746b
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-10 17:02:53 -0600

    Test 8 venus Years Passed good fail

---
+++ __test__/EarthAge.test.js
@@ -0,0 +1,35 @@
+import { EarthAge } from '../src/js/EarthAge.js'
+
+describe('EarthAge', () => {
+    let newAge;
+    
+    beforeEach(() => {
+        newAge = new EarthAge(37,25,50);
+    });
+    test('should create a variable for earthYears', () =>{
+            expect(newAge.earthYears).toBe(37);
+            expect(newAge.pastAge).toBe(25);
+            expect(newAge.futureAge).toBe(50);
+    });
+    test('should return newAge in Mercury years', () =>{
+        expect(newAge.ageOnMercury()).toBe(9);
+    });
+        test('should return newAge in Venus years', () =>{
+            expect(newAge.ageOnVenus()).toBe(23);
+    });
+    test('should return newAge in Mars years', () =>{
+        expect(newAge.ageOnMars()).toBe(20);
+    });
+    test('should return newAge in Jupiter years', () =>{
+        expect(newAge.ageOnJupiter()).toBe(3);
+    });
+   test('should subtract past age from earthYears and return EarthYears Passed', () =>{
+     expect(newAge.earthYearsPassed()).toBe("12 Earth years have passed");
+    });
+    test('should subtract past age from earthYears and return mercury Years passed', () =>{
+        expect(newAge.mercuryYearsPassed()).toBe("3 Mercury years have passed");
+    });
+    test('should subtract past age from earthYears and return Venus Years passed', () =>{
+        expect(newAge.venusYearsPassed()).toBe("7 Venus years have passed");
+    });
+});
---
+++ src/js/EarthAge.js
@@ -0,0 +1,30 @@
+export class EarthAge {
+    constructor(earthYears, pastAge, futureAge) {
+        this.earthYears = earthYears;
+        this.pastAge = pastAge;
+        this.futureAge = futureAge;
+    }
+    ageOnMercury() {
+        return Math.round(this.earthYears * 0.24);
+    }
+    ageOnVenus() {
+        return Math.round(this.earthYears * 0.62);
+    }
+    ageOnMars() {
+        return Math.round(this.earthYears / 1.88);
+    }
+    ageOnJupiter() {
+        return Math.round(this.earthYears / 11.86);
+    }
+    earthYearsPassed() {
+        let ageDiff = this.earthYears - this.pastAge;
+        return ageDiff + " Earth years have passed"
+    }
+    mercuryYearsPassed() {
+        let ageDiff = this.earthYears - this.pastAge; 
+        return Math.round(ageDiff * 0.24) + " Mercury years have passed"
+    }
+    venusYearsPassed() {
+        
+    }
+}
