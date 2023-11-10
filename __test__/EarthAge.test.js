7d156d65941a4e88561ee0b8e2cfa8452557abe1
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-10 11:16:22 -0600

    Test 1 passed after I rephrased and figured out order.

--- __test__/EarthAge.test.js
+++ __test__/EarthAge.test.js
@@ -1,7 +1,8 @@
-import {EarthAge} from './src/js/EarthAge.js'
+import { EarthAge } from '../src/js/EarthAge.js'
 
 describe('EarthAge', () => {
-    test('should create a variable for earthYears that returns current Age')
+    test('should create a variable for earthYears', () =>{
     let newAge = new EarthAge(37);
-    expect(earthYears).toEqual(37);
+    expect(newAge.earthYears).toBe(37);
+});
 });
--- src/js/EarthAge.js
+++ src/js/EarthAge.js
@@ -1,4 +1,5 @@
 export class EarthAge {
     constructor(earthYears) {
+        this.earthYears = earthYears;
     }
 }
