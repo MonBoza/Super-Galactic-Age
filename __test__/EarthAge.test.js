6780054940228c278e9b7d34ba849ce6d4138a4b
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-10 12:22:29 -0600

    test5 age on Jupiter test written

--- __test__/EarthAge.test.js
+++ __test__/EarthAge.test.js
@@ -4,19 +4,24 @@ describe('EarthAge', () => {
     
    
     test('should create a variable for earthYears', () =>{
-        let newAge = new EarthAge(37);
-        expect(newAge.earthYears).toBe(37);
-});
-test('method should return newAge in Mercury years', () =>{
     let newAge = new EarthAge(37);
-    expect(newAge.ageOnMercury()).toBe(9);
-});
-    test(' method should return newAge in Venus years', () =>{
+            expect(newAge.earthYears).toBe(37);
+    });
+    test('method should return newAge in Mercury years', () =>{
         let newAge = new EarthAge(37);
-        expect(newAge.ageOnVenus()).toBe(23);
-});
+        expect(newAge.ageOnMercury()).toBe(9);
+    });
+        test(' method should return newAge in Venus years', () =>{
+            let newAge = new EarthAge(37);
+            expect(newAge.ageOnVenus()).toBe(23);
+    });
     test('method should return newAge in Mars years', () =>{
         let newAge = new EarthAge(37);
         expect(newAge.ageOnMars()).toBe(20);
     });
+    test('method should return newAge in Jupiter years', () =>{
+        let newAge = new EarthAge(37);
+        expect(newAge.ageOnJupiter()).toBe(3);
+    });
+    
 });
