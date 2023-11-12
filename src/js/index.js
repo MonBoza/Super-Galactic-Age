94773413a55d327a4808a6ddc1d7004871250c4c
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-12 14:36:16 -0600

    new testfile and updated .js CSS html

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
-// import { EarthAge } from './EarthAge.js';
+import { EarthAge2 } from './EarthAge2.js';
 
