cc8149316a874172d62590e4a3a366794d9215aa
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-12 11:52:51 -0600

    "Test1 passed"

---
+++ src/js/EarthAge2.js
@@ -0,0 +1,7 @@
+export class EarthAge2 {
+    constructor(earthYears, pastAge, futureAge) {
+        this.earthYears = earthYears;
+        this.pastAge = pastAge;
+        this.futureAge = futureAge;
+    }
+} 
