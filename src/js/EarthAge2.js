216ae5d9d74b3b0c2440655bf3b07929c258db93
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-12 12:48:01 -0600

    test 3 passed

--- src/js/EarthAge2.js
+++ src/js/EarthAge2.js
@@ -1,7 +1,13 @@
 export class EarthAge2 {
-    constructor(earthYears, pastAge, futureAge) {
-        this.earthYears = earthYears;
+    constructor(currentAge, pastAge, futureAge) {
+        this.currentAge = currentAge;
         this.pastAge = pastAge;
         this.futureAge = futureAge;
+    }
+    calculateAge(planet, age) {
+        return parseInt(age / planet); 
+    }
+    mercuryAge() {
+        return this.calculateAge(0.24, this.currentAge);
     }
 } 
