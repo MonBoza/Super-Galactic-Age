2795fe8b0a4fb59b1560ca72cde69a7978bd8c6b
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-12 12:10:06 -0600

    "test2 calculatePlanet good fail"

--- __test__/EarthAge2.test.js
+++ __test__/EarthAge2.test.js
@@ -8,8 +8,11 @@ describe('EarthAge2', () => {
         newAge = new EarthAge2(37,25,50);
     });
     test('should create object with three ages currentAge, futureAge, and pastAge', () => {
-        expect(newAge.earthYears).toBe(37);
+        expect(newAge.currentAge).toBe(37);
         expect(newAge.pastAge).toBe(25);
         expect(newAge.futureAge).toBe(50);
+    });
+    test('should calculate currentAge on different planet years', () => {
+        expect(newAge.calculatePlanets()).toBe("Mercury: 8.88 Venus: 22.94 Mars: 1968 Jupiter: 3.12");
     });
 });
